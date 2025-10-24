ca067e1d3574962961639e751580811ef318e832
Dunsin <ayodejiabisuwa23@gmail.com>
2025-10-24 15:03:04 +0100

    feat:  server ready

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Dunsin-cyber/rssagg
 
 go 1.25.3
+
+require (
+	github.com/go-chi/chi/v5 v5.2.3
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+)
 
 func main () {
-	fmt.Println("hello world")
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+
+	serve := &http.Server{
+		Handler: router,
+		Addr: ":" + portString,
+	}
+
+	log.Printf("Server started on port %v 🎉🐱‍🏍", portString)
+	err := serve.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
+
 }
