c976243f480fbf06ce2c15798263286e9013d951
Dunsin <ayodejiabisuwa23@gmail.com>
2025-10-24 16:23:41 +0100

    feat: boilerplate for http server complete

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/joho/godotenv"
 )
@@ -23,12 +24,27 @@ func main () {
 	router.Use(middleware.Logger)
 	router.Use(middleware.Recoverer)
 
+	router.Use(cors.Handler(cors.Options{ 
+    AllowedOrigins:   []string{"https://*", "http://*"},
+    AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+    AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+    ExposedHeaders:   []string{"Link"},
+    AllowCredentials: false,
+    MaxAge:           300, 
+	}))
+
+	v1Router :=  chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1Router)
+
 	serve := &http.Server{
 		Handler: router,
 		Addr: ":" + portString,
 	}
 
-	log.Printf("Server started on port %v 🎉🐱‍🏍", portString)
+	log.Printf("Server started on port %v 🎉", portString)
 	err := serve.ListenAndServe()
 		if err != nil {
 			log.Fatal(err)
